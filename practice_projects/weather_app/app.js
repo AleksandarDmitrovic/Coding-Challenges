0295095f9ec767f60072561df65415a38df71b5a
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-01-05 15:06:43 -0700

    weather api setup -weather app

--- practice_projects/weather_app/app.js
+++ practice_projects/weather_app/app.js
@@ -6,7 +6,19 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${long}}&appid={4c904cde9536b550f6c533d7e33f8296}`
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
     });
+
   } else {
     h1.textContent = "Please enable location services"
   }
