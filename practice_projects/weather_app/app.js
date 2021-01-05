ec69b9cd475c2be95b153dab7f337ab3f7888bcd
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-01-05 15:57:05 -0700

    pull data from api call -weather app

--- practice_projects/weather_app/app.js
+++ practice_projects/weather_app/app.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -8,7 +11,7 @@ window.addEventListener('load', () => {
       lat = position.coords.latitude;
 
       const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${long}}&appid={4c904cde9536b550f6c533d7e33f8296}`
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4c904cde9536b550f6c533d7e33f8296`
 
       fetch(api)
         .then(response => {
@@ -16,6 +19,13 @@ window.addEventListener('load', () => {
         })
         .then(data => {
           console.log(data);
+          const { temp } = data.main;
+          const summary = data.weather[0].description
+
+          //Set DOM Elements from the API
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.name
         })
     });
 
